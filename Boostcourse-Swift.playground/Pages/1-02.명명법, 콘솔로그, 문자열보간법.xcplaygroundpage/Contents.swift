42bef4cd905b4268e66b6a3dc3a92c2f2b88dbb7
Sang Hyeok Han <contea95@naver.com>
2021-03-28 21:44:24 +0900

    1_02.명명법/콘솔로그/문자보간법

---
+++ Boostcourse-Swift.playground/Pages/1-02.명명법, 콘솔로그, 문자열보간법.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+import Swift
+
+let age : Int = 10
+
+"안녕하세요! 저는 \(age)살 입니다."
+
+"안녕하세요! 저는 \(age+5)살 입니다."
+
+print("안녕하세요! 저는 \(age+5)살 입니다.")
+
+print("\n#########################\n")
+    
+class Person {
+    var name:String = "han"
+    var age: Int = 10
+}
+
+let han: Person = Person()
+
+print(han)
+
+print("\n###########################\n")
+
+dump(han)
