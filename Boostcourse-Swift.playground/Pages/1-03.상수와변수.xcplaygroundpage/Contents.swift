0f7a83b83f5a6905ca2e16056437b592b0ca6f4f
Sang Hyeok Han <contea95@naver.com>
2021-03-29 16:05:56 +0900

    1_03.상수와 변수

---
+++ Boostcourse-Swift.playground/Pages/1-03.상수와변수.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+// 상수 변수 선언
+let 상수이름: 타입 = 값
+var 변수이름: 타입 = 값
+
+// 값의 타입이 명확하면 생략 가능
+let 상수이름 = 값
+var 변수이름 = 값
+
+let constant: String = "차후에 변경 불가능한 상수 let"
+var variable = String = "차후에 변경 가능한 변수 var"
+
+variable = "변수에 다른 값 할당할 수 있지만"
+//constant = "상수는 다른 값을 변경할 수 없다."
+
+let sum:Int
+let InputA:Int = 100
+let InputB:Int = 200
+
+sum = InputA + InputB
+
+//sum = 1 // 이후 값 할당 불가능
+
+var nickName: String
+
+nickName = "han"
+
+nickName = "한"
